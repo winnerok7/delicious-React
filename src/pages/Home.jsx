e28416adb7318a2a7c5eec653dbc400624f855d8
winnerok7 <winnerok7@gmail.com>
2022-07-06 11:35:50 +0200

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Popular from "../components/Popular";
+import Veggie from "../components/Veggie";
+
+
+import React from 'react'
+
+function Home() {
+   return (
+      <div>
+         <Veggie />
+         <Popular />
+      </div>
+
+   )
+}
+
+export default Home
